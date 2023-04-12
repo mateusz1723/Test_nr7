af80cac0255ef9fcbec2d3616d6eef5ad5f7f922
mateusz.trochimowicz <trohimowicz@onet.pl>
2023-04-12 11:07:29 +0200

    Added mappers to dto

---
+++ src/main/java/pl/kurs/shape_api/mapper/ShapeMapper.java
@@ -0,0 +1,28 @@
+package pl.kurs.shape_api.mapper;
+
+import org.springframework.stereotype.Service;
+import pl.kurs.shape_api.dto.ShapeDto;
+import pl.kurs.shape_api.models.Shape;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+
+@Service
+public class ShapeMapper {
+
+
+    private final Map<String, ShapeDtoMapper> mappers;
+
+    public ShapeMapper(Set<ShapeDtoMapper> mappers){
+        this.mappers = mappers.stream().collect(Collectors.toMap(ShapeDtoMapper::getType, Function.identity()));
+    }
+
+    public ShapeDto mapToDto(Shape shape){
+        return mappers.get(shape.getType().name()).map(shape);
+    }
+
+
+}
