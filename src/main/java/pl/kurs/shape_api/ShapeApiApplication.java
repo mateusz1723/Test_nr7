5f14b3360acbc86ae3e6f7a5dbe1ac6dff2841c4
mateusz.trochimowicz <trohimowicz@onet.pl>
2023-04-12 11:15:23 +0200

    Added application_properties and api

---
+++ src/main/java/pl/kurs/shape_api/ShapeApiApplication.java
@@ -0,0 +1,13 @@
+package pl.kurs.shape_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShapeApiApplication.class, args);
+    }
+
+}
