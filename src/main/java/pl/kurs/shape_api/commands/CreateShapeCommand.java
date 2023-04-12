67e1ba40cfd0e5161905dad14f94b0fc46b98efe
mateusz.trochimowicz <trohimowicz@onet.pl>
2023-04-12 10:58:49 +0200

    Added createShapeCommand class

---
+++ src/main/java/pl/kurs/shape_api/commands/CreateShapeCommand.java
@@ -0,0 +1,24 @@
+package pl.kurs.shape_api.commands;
+
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.Map;
+
+public class CreateShapeCommand {
+
+    @NotBlank
+    private String type;
+    @NotEmpty
+    private Map<String, Object> parameters;
+
+    public String getType() {
+        return type;
+    }
+
+    public Map<String, Object> getParameters() {
+        return parameters;
+    }
+
+}
+
