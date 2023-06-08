823104c7d702d57f806e71ef02a376bcc8e19aa4
mateusz.trochimowicz <trohimowicz@onet.pl>
2023-06-08 12:42:36 +0200

    Delete NotUniqueUsernameException class and VersionNotEqualsException

--- src/main/java/pl/kurs/shape_api/exceptionhandling/VersionNotEqualsException.java
+++
@@ -1,8 +0,0 @@
-package pl.kurs.shape_api.exceptionhandling;
-
-public class VersionNotEqualsException extends Exception{
-
-    public VersionNotEqualsException(String message) {
-        super(message);
-    }
-}
